2f789acee68a876ff27c03a4f8bd7ebde71d9e12
Lukas Alexandre <lukas@codelogic.me>
2015-05-06 20:49:50 +0200

    Adding better styling for the app docs

---
+++ app/controllers/application.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    invalidateSession: function() {
+      this.get('session').invalidate();
+      //this.transitionToRoute('login');
+    }
+  }
+});
