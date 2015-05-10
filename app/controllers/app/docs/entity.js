77af4b8a59e44bc43ea07357f24c45eda1f14948
Lukas Alexandre <lukas@codelogic.me>
2015-05-10 14:24:21 +0200

    Improving API documentation

--- app/controllers/app/docs/entity.js
+++ app/controllers/app/docs/entity.js
@@ -4,6 +4,6 @@ export default Ember.Controller.extend({
   publicFields: function() {
     return this.model.get('fields').filter(function(field) {
       return !field.get('internal');
-    })
+    });
   }.property()
-})
+});
